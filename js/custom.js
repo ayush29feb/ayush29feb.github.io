2e50abea4e5a0d193e596093e89a2da71122ed7d
Ayush <ayush29f@uw.edu>
2015-01-29 01:12:14 -0800

    A litle bit more stuff on card

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 $(function(){
-  $(".typer").typed({
-    strings: ["Hey, I am Ayush!", "I am a developer", "I am a designer"],
-    typeSpeed: 10
+  $(".card h1").typed({
+    strings: ["Ayush Saraf"],
+    typeSpeed: 100
   });
 });
